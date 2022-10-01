8a35070699a3d3eee4d550ed93094044f395a9c6
Luis Barroso <luis.barroso@rootitplatform.com>
2022-10-01 20:10:57 +0200

    Updated user updated event

--- events/user_updated_event.go
+++ events/user_updated_event.go
@@ -1,10 +1,12 @@
 package events
 
+import "github.com/PlayEconomy37/Play.Common/permissions"
+
 // UserUpdatedEvent is the event sent whenever an user is created or updated
 type UserUpdatedEvent struct {
-	ID          int64    `json:"id"`
-	Email       string   `json:"email"`
-	Permissions []string `json:"permissions"`
-	Activated   bool     `json:"activated"`
-	Version     int32    `json:"version"`
+	ID          int64                   `json:"id"`
+	Email       string                  `json:"email"`
+	Permissions permissions.Permissions `json:"permissions"`
+	Activated   bool                    `json:"activated"`
+	Version     int32                   `json:"version"`
 }
